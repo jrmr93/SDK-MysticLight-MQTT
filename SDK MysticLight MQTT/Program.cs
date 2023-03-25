131a5afd748c6d732c68cf99fce4abf07edcbd29
jrmr93 <jrmr93@gmail.com>
2023-03-25 00:00:35 -0500

    3

--- SDK MysticLight MQTT/Program.cs
+++ SDK MysticLight MQTT/Program.cs
@@ -6,7 +6,7 @@ namespace MysticLightController
     {
         static void Main(string[] args)
         {
-            LightController controller = new LightController();
+            LightController controller = new LightController("s");
             Console.WriteLine("Se encontraron " + controller.Devices.Length + " dispositivos");
             int i = 0;
             foreach (string dev in controller.Devices)
